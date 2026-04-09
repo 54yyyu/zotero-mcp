d3e19bf921552b1b51aa6c6e98b16e2541bf4cb7
lots-o <39071632+lots-o@users.noreply.github.com>
2026-04-10 05:54:00 +0900

    Merge PR #190: add PDF area annotation tool

--- src/zotero_mcp/server.py
+++ src/zotero_mcp/server.py
@@ -92,6 +92,7 @@ from zotero_mcp.tools.annotations import (  # noqa: F401
     update_note,
     delete_note,
     create_annotation,
+    create_area_annotation,
 )
 from zotero_mcp.tools.write import (  # noqa: F401
     batch_update_tags,
